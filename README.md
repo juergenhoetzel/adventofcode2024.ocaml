0d121ca6913b20c45835b1da8051d11825e984df
Juergen Hoetzel <juergen@hoetzel.info>
2024-12-03 21:45:43 +0100

    Add Day 2: part1

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | Advent of Code Page                                              | Part 1 | Part 2 | Day                    |
 |------------------------------------------------------------------|--------|--------|------------------------|
 | [Day 1: Historian Hysteria](https://adventofcode.com/2024/day/1) | ✅     | ✅     | [day1.ml](lib/day1.ml) |
+| [Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2)  | ✅     | ❌     | [day2.ml](lib/day2.ml) |
 | [Day 3: Mull It Over](https://adventofcode.com/2024/day/3)       | ✅     | ✅     | [day3.ml](lib/day3.ml) |
 
 
--- test/test_aoc.ml
+++ test/test_aoc.ml
@@ -9,7 +9,10 @@ let () =  Alcotest.run "Aoc test suite"
                          "Day1 part 2", `Quick, (fun () -> (check int "Expected int" 31 (Aoc.Day1.part2 testdata)));
                        ]
               );
-              ("Day2", let testdata  = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" in
+              ("Day2", let testdata = ["7 6 4 2 1";"1 2 7 8 9";"9 7 6 2 1";"1 3 2 4 5";"8 6 4 4 1";"1 3 6 7 9"] in
+                       ["Day2 part 1", `Quick, (fun () -> (check int "Expected int" 2 (Aoc.Day2.part1 testdata)));]
+              );
+              ("Day3", let testdata  = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" in
                        [
                          "Day3 part 1", `Quick, (fun () -> (check int "Expected int" 161 (Aoc.Day3.part1 testdata)));
                          "Day3 part 2", `Quick, (fun () -> (check int "Expected int" 48 (Aoc.Day3.part2 testdata)));
