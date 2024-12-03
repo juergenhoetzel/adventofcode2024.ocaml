f6b0f3edc785a52f6b5b9179a1e96cea4691e5b7
Juergen Hoetzel <juergen@hoetzel.info>
2024-12-03 20:35:10 +0100

    Add Day 3

--- bin/main.ml
+++ bin/main.ml
@@ -2,8 +2,10 @@ let () = match Sys.argv with
   | [| _; day_s; file_name |] -> (match (int_of_string_opt day_s) with
                                   | Some 1 -> let input = Aoc.Day1.read_lists file_name in
                                               Format.printf "Part1: %d\nPart2: %d\n" (Aoc.Day1.part1 input) (Aoc.Day1.part2 input)
-                                  | Some _ -> Format.printf "Unknown Day: %s" day_s
-                                  | None -> Format.printf "Invalid day syntax: %s" day_s)
+                                  | Some 3 -> let concated_input = In_channel.with_open_text file_name (In_channel.fold_lines (^) "") in
+                                              Format.printf "Part1: %d\nPart2: %d\n" (Aoc.Day3.part1 concated_input) (Aoc.Day3.part2 concated_input)
+                                  | Some _ -> Format.printf "Unknown Day: %s\n" day_s
+                                  | None -> Format.printf "Invalid day syntax: %s\n" day_s)
   | _ -> Format.printf "Usage: %s day input_file\n" Sys.argv.(0)
 
 
--- test/test_aoc.ml
+++ test/test_aoc.ml
@@ -1,16 +1,17 @@
 open Alcotest
 
 
-
-let day1_input = ([1; 2; 3; 3; 3; 4], [3; 3; 3; 4; 5; 9])
-
-let day1_suite =
-  [
-    "Day1 part 1", `Quick, (fun () -> (check int "Expected int" 11 (Aoc.Day1.part1 day1_input)));
-    "Day1 part 2", `Quick, (fun () -> (check int "Expected int" 31 (Aoc.Day1.part2 day1_input)));
-  ]
-
 let () =  Alcotest.run "Aoc test suite"
-[
-  ("Day1", day1_suite);
-]
+            [
+              ("Day1", let testdata = ([1; 2; 3; 3; 3; 4], [3; 3; 3; 4; 5; 9]) in
+                       [
+                         "Day1 part 1", `Quick, (fun () -> (check int "Expected int" 11 (Aoc.Day1.part1 testdata)));
+                         "Day1 part 2", `Quick, (fun () -> (check int "Expected int" 31 (Aoc.Day1.part2 testdata)));
+                       ]
+              );
+              ("Day2", let testdata  = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" in
+                       [
+                         "Day3 part 1", `Quick, (fun () -> (check int "Expected int" 161 (Aoc.Day3.part1 testdata)));
+                         "Day3 part 2", `Quick, (fun () -> (check int "Expected int" 48 (Aoc.Day3.part2 testdata)));
+              ]);
+            ]
